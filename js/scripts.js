d2c95f73f2f417455df68996bee1165ac75cd67f
Kelsey Langlois and Safiy Cham <kelsey@>
2017-08-22 14:39:03 -0700

    add list of events and print when prompted.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
+function Event (name, date) {
+  this.name = name;
+  this.date = date;
+}
+
+
+
+
+
 $(document).ready(function() {
   $("form#event").submit(function(event) {
     event.preventDefault();
     var inputEvent = $("input#event-name").val();
     var inputDate = $("input#event-date").val();
 
-    console.log(inputEvent, inputDate);
+    var newEvent = new Event(inputEvent, inputDate);
+    console.log(newEvent);
+    $("ul.events").append("<li><span class='event'>" + newEvent.date + "</span></li>");
 
+    $(".event").last().click(function() {
+      $(".display").show();
+      $(".display").text(newEvent.name + " "  + newEvent.date);
+
+    });
   });
 });
