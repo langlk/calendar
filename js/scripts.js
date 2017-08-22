091c679104bcbee2e300d08772c7abe13c80487b
Kelsey Langlois and Safiy Cham <kelsey@>
2017-08-22 15:15:04 -0700

    Update to display days in order, and show all events for a day when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,48 @@ function Event (name, date) {
   this.date = date;
 }
 
-
-
-
+// Stores all days that we have events for
+var days = {};
 
 $(document).ready(function() {
   $("form#event").submit(function(event) {
     event.preventDefault();
+    // Get input and store to variables
     var inputEvent = $("input#event-name").val();
     var inputDate = $("input#event-date").val();
 
-    var newEvent = new Event(inputEvent, inputDate);
-    console.log(newEvent);
-    $("ul.events").append("<li><span class='event'>" + newEvent.date + "</span></li>");
+    if (!inputEvent || !inputDate) {
+      alert("Invalid Input");
+    } else {
+      // Make a new event to store the input
+      var newEvent = new Event(inputEvent, inputDate);
 
-    $(".event").last().click(function() {
-      $(".display").show();
-      $(".display").text(newEvent.name + " "  + newEvent.date);
+      // If our list of days has this day already, we add the event to that day. If not, we add an entry for this day
+      if (days.hasOwnProperty(inputDate)) {
+        days[inputDate].push(newEvent);
+      } else {
+        days[inputDate] = [newEvent];
+      }
 
-    });
+
+      $("ul.events").empty();
+      // Get all the keys in days (which should be all of our dates), then sort them, and list them in order on the page.
+      Object.keys(days).sort().forEach(function(date) {
+        $("ul.events").append("<li><span class='day-" + date + "'>" + date + "</span></li>");
+
+        // Add a click listener to each date
+        $(".day-" + date).click(function() {
+          $(".display").show();
+          $(".display").empty();
+          $(".day").text(date);
+
+          // Show all events attatched to this day
+          days[date].forEach(function(dayEvent) {
+            $(".display").append("<li>" + dayEvent.name + "</li>");
+          });
+        });
+      });
+      $("input").val("");
+    }
   });
 });
